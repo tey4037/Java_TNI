fc11564f53dd8811383d0f3632e18ef84b4d368b
Sukolwat <tey4037@gmail.com>
2025-11-18 13:49:22 +0700

    summit exercise LabSheet1

---
+++ labsheet01/src/Lab101.java
@@ -0,0 +1,8 @@
+public class Ex102 {
+    public static void main(String[] args) {
+        System.out.println("im Sukolwat homjan");
+        System.out.println("\"INT-108\" Object-Oriented Programming");
+        System.out.println("\tthis is my frist time learning Java");
+        System.out.println("I hope to get grade \"A\"");
+    }
+}
